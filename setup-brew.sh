c0300e9815394a4caeb2719675a666d2cb5dd12e
Navdeep Singh <navdeepsinghkhalsa@icloud.com>
2023-07-17 07:09:54 -0700

    Remove uncommonly-used apps

--- setup-brew.sh
+++ setup-brew.sh
@@ -5,31 +5,10 @@ brew install ffmpeg # required for youtube-dl
 brew install youtube-dl
 
 # Applications
-brew cask install 1password
-brew cask install android-studio
-brew cask install appcleaner
-brew cask install authy
-brew cask install clipy
-brew cask install dash
-brew cask install docker
-brew cask install firefox
-brew cask install fork
-brew cask install google-chrome
-brew cask install macdown
-brew cask install parallels
-brew cask install parallels-toolbox
-brew cask install postman
-brew cask install realm-browser
-brew cask install sequel-pro
-brew cask install slack
-brew cask install spark
-brew cask install tableplus
-brew cask install telegram-desktop
-brew cask install the-unarchiver
-brew cask install tripmode
-brew cask install vanilla
-brew cask install viscosity
-brew cask install visual-studio-code
+brew install --cask 1password
+brew install --cask appcleaner
+brew install --cask telegram
+brew install --cask visual-studio-code
 
 # Fonts
 brew tap caskroom/fonts
